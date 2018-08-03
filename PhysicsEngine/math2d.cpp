cddb7f1dc1c08f7c753c92e160f21071476a0c80
Mrwanghao <845811521@qq.com>
2018-08-03 19:11:48 +0800

    添加碰撞

--- PhysicsEngine/math2d.cpp
+++ PhysicsEngine/math2d.cpp
@@ -3,6 +3,61 @@
 #include <math.h>
 #include "Matrix2.h"
 
+Interval2D GetInterval(const Rectangle2D & rect, const Vec2 & axis)
+{
+	Interval2D result;
+	Vec2 min = rect.GetMin();
+	Vec2 max = rect.GetMax();
+
+	Vec2 verts[] =
+	{
+		Vec2(min.x, min.y), Vec2(min.x, max.y),
+		Vec2(max.x, max.y), Vec2(max.x, min.y)
+	};
+
+	result.min = result.max = axis.Dot(verts[0]);
+
+	for (int i = 1; i < 4; i++)
+	{
+		float projection = axis.Dot(verts[i]);
+
+		if (projection < result.min)
+		{
+			result.min = projection;
+		}
+
+		if (projection > result.max)
+		{
+			result.max = projection;
+		}
+
+	}
+
+	return result;
+}
+
+bool OverlapOnAxis(const Rectangle2D & rect1, const Rectangle2D & rect2, const Vec2 & axis)
+{
+	Interval2D a = GetInterval(rect1, axis);
+	Interval2D b = GetInterval(rect2, axis);
+	return b.min <= a.max && a.min <= b.max;
+}
+
+bool RectangleRectangleSAT(const Rectangle2D & rect1, const Rectangle2D & rect2)
+{
+	Vec2 axisToTest[] = { Vec2(1, 0), Vec2(0, 1) };
+
+	for (int i = 0; i < 2; i++)
+	{
+		if (!OverlapOnAxis(rect1, rect2, axisToTest[i]))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
 bool Point2DOnLine2D(const Point2D & _point, const Line2D & _line2d)
 {
 	Vec2 offset = _line2d.end - _line2d.start;
@@ -111,4 +166,67 @@ bool LineOrientedRectangle(const Line2D & line, const OrientedRectangle2D & rect
 
 	Rectangle2D localRectangle(Point2D(), rectangle.halfExtents * 2.0f);
 	return LineRectangle(localLine, localRectangle);
+}
+
+bool CircleCircle(const Circle2D & c1, const Circle2D & c2)
+{
+	Line2D line(c1.center, c2.center);
+	float radiusSum = c1.radius + c2.radius;
+	return line.LengthSq() <= radiusSum;
+}
+
+bool CircleRectangle(const Circle2D & circle, const Rectangle2D & rectangle)
+{
+	Vec2 min = rectangle.GetMin();
+	Vec2 max = rectangle.GetMax();
+
+	Point2D closestPoint = circle.center;
+
+	closestPoint.x = closestPoint.x < min.x ? min.x : closestPoint.x;
+	closestPoint.x = closestPoint.x > max.x ? max.x : closestPoint.x;
+
+	closestPoint.y = closestPoint.y < min.y ? min.y : closestPoint.y;
+	closestPoint.y = closestPoint.y > max.y ? max.y : closestPoint.y;
+
+	Line2D line(circle.center, closestPoint);
+
+	return line.LengthSq() <= circle.radius * circle.radius;
+}
+
+bool CircleOrientedRectangle(const Circle2D & circle, const OrientedRectangle2D & orientedRectangle)
+{
+	Vec2 r = circle.center - orientedRectangle.origin;
+
+	float theta = -DEG2RAD(orientedRectangle.rotation);
+	Matrix2 zRotation(cosf(theta), sinf(theta), -sinf(theta), cos(theta));
+
+	r = r * zRotation;
+
+	Circle2D localCircle(r + orientedRectangle.halfExtents, circle.radius);
+	Rectangle2D localRectangle(Point2D(), orientedRectangle.halfExtents * 2);
+
+	return CircleRectangle(localCircle, localRectangle);
+}
+
+bool RectangleRectangle(const Rectangle2D & rect1, const Rectangle2D & rect2)
+{
+	Vec2 aMin = rect1.GetMin();
+	Vec2 aMax = rect1.GetMax();
+	Vec2 bMin = rect2.GetMin();
+	Vec2 bMax = rect2.GetMax();
+
+	bool overX = (bMin.x <= aMax.x && aMin.x <= bMax.x);
+	bool overY = (bMin.y <= aMax.y && aMin.y <= bMax.y);
+
+	return overX && overY;
+}
+
+bool RectangleToOrientedRectangle(const Rectangle2D & rect, const OrientedRectangle2D & orientedRectangle)
+{
+	return false;
+}
+
+bool OrientedRectangleToOrientedRectangle(const OrientedRectangle2D & orientedRectangle1, const OrientedRectangle2D & orientedRectangle2)
+{
+	return false;
 }
--- PhysicsEngine/math2d.h
+++ PhysicsEngine/math2d.h
@@ -5,6 +5,16 @@
 #include "Rectangle2D.h"
 #include "OrientedRectangle2D.h"
 
+class Interval2D
+{
+public:
+	float min;
+	float max;
+};
+
+Interval2D GetInterval(const Rectangle2D& rect, const Vec2& axis);
+bool OverlapOnAxis(const Rectangle2D& rect1, const Rectangle2D& rect2, const Vec2& axis);
+
 bool Point2DOnLine2D(const Point2D& _point, const Line2D& _line2d);
 bool Point2DInCircle2D(const Point2D& _point, const Circle2D& _circle2d);
 bool Point2DInRectangle2D(const Point2D& _point, const Rectangle2D& _rect2d);
@@ -14,3 +24,10 @@ bool LineCircle(const Line2D& line, const Circle2D& circle);
 bool LineRectangle(const Line2D& l, const Rectangle2D& r);
 bool LineOrientedRectangle(const Line2D& line,
 	const OrientedRectangle2D& rectangle);
+
+bool CircleCircle(const Circle2D& c1, const Circle2D& c2);
+bool CircleRectangle(const Circle2D& circle, const Rectangle2D& rectangle);
+bool CircleOrientedRectangle(const Circle2D& circle, const OrientedRectangle2D& orientedRectangle);
+bool RectangleRectangle(const Rectangle2D& rect1, const Rectangle2D& rect2);
+bool RectangleToOrientedRectangle(const Rectangle2D& rect, const OrientedRectangle2D& orientedRectangle);
+bool OrientedRectangleToOrientedRectangle(const OrientedRectangle2D& orientedRectangle1, const OrientedRectangle2D& orientedRectangle2);
